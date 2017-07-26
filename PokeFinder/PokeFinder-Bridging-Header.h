e0ffd42b49e4040e3e7ec769d226ef2fe020c669
Caroline Davis <caroline_davis@live.com>
2017-07-26 16:21:36 +1000

    first commit

---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Caroline Davis on 26/07/2017.
+//  Copyright © 2017 Caroline Davis. All rights reserved.
+//
+
+#import "GeoFire.h"
